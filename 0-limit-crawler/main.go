6b0ce3a1f871af07ae45ea98078a8e26cc07354e
yuming <qym316@gmail.com>
2020-05-11 22:30:50 -0700

    provided two solutions

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,17 +12,59 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+// // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
+// // real crawler. It crawls until the maximum depth has reached.
+// func Crawl(url string, depth int, wg *sync.WaitGroup, limiter chan int) {
+// 	fmt.Println(fmt.Sprintf("depth: %d", depth))
+// 	<-time.Tick(time.Second)
+// 	<-limiter
+
+// 	defer wg.Done()
+
+// 	if depth <= 0 {
+// 		return
+// 	}
+
+// 	body, urls, err := fetcher.Fetch(url)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	fmt.Printf("found: %s %q\n", url, body)
+
+// 	wg.Add(len(urls))
+// 	for _, u := range urls {
+// 		// Do not remove the `go` keyword, as Crawl() must be
+// 		// called concurrently
+// 		limiter <- 1
+// 		go Crawl(u, depth-1, wg, limiter)
+// 	}
+// 	return
+// }
+
+// func main() {
+// 	var wg sync.WaitGroup
+// 	wg.Add(1)
+// 	limiter := make(chan int, 1)
+// 	limiter <- 1
+// 	Crawl("http://golang.org/", 4, &wg, limiter)
+// 	wg.Wait()
+// }
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle <-chan time.Time) {
+	fmt.Println(fmt.Sprintf("depth: %d", depth))
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
-
+	<-throttle
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
@@ -35,15 +77,15 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, throttle)
 	}
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
-
+	throttle := time.Tick(time.Second)
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
